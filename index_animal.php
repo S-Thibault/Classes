439cf3497022c8a79ae90708f1fee5f144ed08f5
S-Thibault <sylvain.thibault@infamail.com>
2020-08-31 15:39:29 +0200

    Update index_animal.php

--- index_animal.php
+++ index_animal.php
@@ -6,18 +6,24 @@
   </head>
   <body>
 
+<?php
+  require 'Animal.class.php';
+  require 'Oiseau_Animal.class.php';
+  require 'Reptile_Animal.class.php';
 
-    <?php
-    class Animal {
-      public $addition;
-      public $soustraction;
-      public $division;
-      public $multiplication;
-      public $modulo;
+(new Animal())->respire();
 
-
-    }
-
+$AcerLeFaucon = new Faucon('Acer', 2);
+echo $AcerLeFaucon->display_infos() . "" . 'ans est un Faucon';
+echo "<br>";
+  $AtomLAigle = new Aigle('Atom','1500');
+  echo $AtomLAigle->display_infos() . "" . 'ans est un Aigle';
+  echo "<br>";
+    $RobertLaTortue = new Tortue('Robert','145');
+    echo $RobertLaTortue->display_infos() . "" . 'ans est une Tortue';
+    echo "<br>";
+      $BobLeDino = new Dinosaure('Bob','25');
+      echo $BobLeDino->display_infos() . "" . 'ans est un Dinosaure';
 
 ?>
 
